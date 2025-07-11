e3092e86cc3ea12d49ce07468a7fdef3db61a581
Felipe Emos <felipe.emos.computacao@gmail.com>
2025-07-11 20:33:55 -0300

    refactor: home design go back button

---
+++ src/features/workspace/components/current-workspace-details-button.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { useRouteWorkspace } from "@/routes/_private/w.$workspaceId/route";
+import { Link } from "@tanstack/react-router";
+import { ChevronDown } from "lucide-react";
+import { WorkspaceAvatar } from "./workspace-avatar";
+import { cn } from "@/lib/utils";
+
+export function CurrentWorkspaceDetailsButton({
+  className,
+}: {
+  className?: string;
+}) {
+  const workspace = useRouteWorkspace();
+
+  return (
+    <Button
+      variant="ghost"
+      className={cn("h-auto justify-start p-3", className)}
+      asChild
+    >
+      <Link
+        to="/w/$workspaceId/config"
+        params={{ workspaceId: workspace.id }}
+        className="flex items-center gap-3 px-8"
+      >
+        <WorkspaceAvatar workspace={workspace} showLocation={false} />
+      </Link>
+    </Button>
+  );
+}
--- src/routes/_private/w.$workspaceId/_nav.home.tsx
+++ src/routes/_private/w.$workspaceId/_nav.home.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { NavLink } from "@/features/navigation/components/nav-link";
-import { CurrentWorkspaceDropdown } from "@/features/workspace/components/current-workspace-dropdown";
-import { createFileRoute } from "@tanstack/react-router";
+import { CurrentWorkspaceDetailsButton } from "@/features/workspace/components/current-workspace-details-button";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { ArrowLeft } from "lucide-react";
 
 export const Route = createFileRoute("/_private/w/$workspaceId/_nav/home")({
   component: RouteComponent,
@@ -11,8 +13,14 @@ function RouteComponent() {
 
   return (
     <div className="flex min-h-svh flex-col">
-      <div className="border-b p-4">
-        <CurrentWorkspaceDropdown />
+      <div className="flex justify-between border-b p-4">
+        <CurrentWorkspaceDetailsButton />
+        <Button asChild>
+          <Link to="/home-root">
+            <ArrowLeft className="mr-2 h-4 w-4" />
+            Sair
+          </Link>
+        </Button>
       </div>
       <div className="flex-1 p-6">
         <NavLink to="/w/$workspaceId/events" params={params}>
